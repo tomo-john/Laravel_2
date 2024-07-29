5058fa70bb22f8a4e488f5a1f0d0feea36fa5266
tomo-john <keririnkery.kery@gmail.com>
2024-07-29 12:41:01 +0900

    add: home

---
+++ src/Laravel-John/resources/views/home.blade.php
@@ -0,0 +1,12 @@
+<!-- resources/views/home.blade.php -->
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Home Page</title>
+</head>
+<body>
+    <h1>Welcome to the Home Page!</h1>
+    <p>This is the new home page.</p>
+</body>
+</html>
+
