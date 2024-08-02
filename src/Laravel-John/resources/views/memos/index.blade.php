0ebe0f8f8659ac054f9737870f2b97ac9239c4b5
tomo-john <keririnkery.kery@gmail.com>
2024-08-02 13:12:35 +0900

    add: css

--- src/Laravel-John/resources/views/memos/index.blade.php
+++ src/Laravel-John/resources/views/memos/index.blade.php
@@ -1,10 +1,10 @@
-<!-- resources/views/memos/index.blade.php -->
-
 @extends('layouts.app')
 
 @section('content')
 <div class="container">
-    <h1>メモ一覧</h1>
+    <h1>Memos</h1>
+    <a href="{{ route('memos.create') }}" class="btn btn-primary mb-3">＋ 新しいメモを作成</a>
+    <a href="{{ route('memos.create') }}" class="btn-cute">＋ 新しいメモを作成</a>
     <ul>
         @foreach($memos as $memo)
             <li>{{ $memo->title }}</li>
