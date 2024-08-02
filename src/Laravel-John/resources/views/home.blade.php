e05b24a5cb0869181368226ba572d0da6b90999c
tomo-john <keririnkery.kery@gmail.com>
2024-08-02 12:45:58 +0900

    add: views

--- src/Laravel-John/resources/views/home.blade.php
+++ src/Laravel-John/resources/views/home.blade.php
@@ -1,14 +1,12 @@
-<!-- resources/views/home.blade.php -->
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Home Page</title>
-</head>
-<body>
+@extends('layouts.app')
+
+@section('content')
+
+<div>
     <h1>Welcome to the Home Page!</h1>
     <p>This is the new home page.</p>
     </br>
     <a href="{{ route('memos.index') }}">メモ一覧を見る</a>
-</body>
-</html>
+</div>
 
+@endsection
--- src/Laravel-John/resources/views/layouts/app.blade.php
+++ src/Laravel-John/resources/views/layouts/app.blade.php
@@ -1,13 +1,39 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Laravel App</title>
-    <!-- ここにCSSやJavaScriptのリンクを追加 -->
+    <title>Laravel John</title>
+    <!-- JavaScript -->
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
+
 <body>
+    <!-- 共通のヘッダー -->
+    <header style="border-bottom: 1px solid #ccc; padding: 10px; display: flex; justify-content: space-between;">
+        <div>
+          <a href="{{ url('/') }}" style="text-decoration: none;">Laravel John</a>
+        </div>
+        <div>
+          <a href="#" style="margin-right: 15px;">じょん1</a>
+          <a href="#" style="margin-right: 15px;">じょん2</a>
+          <a href="#">じょん3</a>
+    </header>
+
+    <!-- 共通のナビゲーション -->
+    <nav>
+
+    </nav>
+
+    <!-- メインコンテンツ(各ビューかれ提供されるコンテンツ) -->
     <div class="container">
         @yield('content')
     </div>
+
+    <!-- 共通のフッター -->
+    <footer>
+
+    </footer>
 </body>
 </html>
 
